2b5aa3b3911fea3b1cc2a589a88f8f22a532f279
Bryce <bykerbry@umich.edu>
2020-04-06 21:39:02 -0400

    all user endpoints requiring auth now have it

--- src/routers/user.js
+++ src/routers/user.js
@@ -58,62 +58,31 @@ router.get('/users/me', auth, async (req, res) => {
     res.send(req.user)
 })
 
-router.get('/users/:id', async (req, res) => {
-    const _id = req.params.id
-
-    try {
-        const user = await User.findById(_id)
-        if (!user) {
-            res.status(404).send()
-        } 
-        res.send(user)
-    } catch (e) {
-        res.status(500).send(e)
-    }
-
-    // User.findById(_id).then((user) => {
-    //     if (!user) {
-    //         return res.status(404).send()
-    //     }
-
-    //     res.send(user)
-    // }).catch((e) => {
-    //     res.status(500).send()
-    // })
-})
-
-router.patch('/users/:id', async (req, res) => {
-    const _id            = req.params.id
+router.patch('/users/me', auth, async (req, res) => {
     const updates        = Object.keys(req.body)
     const allowedUpdates = ['name', 'email', 'password', 'age']
     const isValid        = updates.every( update => allowedUpdates.includes(update))
+
     
     if(!isValid) {
         return res.status(400).send({ error: 'Invalid Request'})
     }
 
     try {
-        const user = await User.findById(_id)
+        const user = req.user
         updates.forEach(update => user[update] = req.body[update])
         await user.save()
-
-        if (!user) {
-            return res.status(404).send()
-        }
         res.send(user)
     
     } catch (e) {
-        res.status(400).send(e)
+        res.status(500).send(e)
     }
 })
 
-router.delete('/users/:id', async (req,res) => {
+router.delete('/users/me', auth, async (req,res) => {
     try {
-        const user = await User.findByIdAndDelete(req.params.id)
-        if (!user) {
-            res.status(404).send()
-        }
-        res.send(user)
+        await req.user.remove()
+        res.send(req.user)
     } catch (e) {
         res.status(500).send()
     }
